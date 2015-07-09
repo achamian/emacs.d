2ab7c464c03885fff592f1f7b4edb693e932e570
Niranjan Paranjape <niranjangp@gmail.com>
2015-07-09 21:22:17 +0530

    Cleaning init.el

--- init.el
+++ init.el
@@ -6,46 +6,25 @@
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 (setq inhibit-startup-message t)
 
-;; Packages should go here
-(setq site-lisp-dir
-      (expand-file-name "site-lisp" user-emacs-directory))
-
-;; Custom lisp
+;; Load path
+;; Package definitions
 (setq settings-dir
       (expand-file-name "lisp" user-emacs-directory))
-
-;; Load path
 (add-to-list 'load-path settings-dir)
-(add-to-list 'load-path site-lisp-dir)
 
-;; Settings for currently logged in user
+;; Custom settings
 (setq user-settings-dir
       (concat user-emacs-directory "users/" user-login-name))
 (add-to-list 'load-path user-settings-dir)
-
-;; Backups go here
-(setq backup-directory-alist
-      `(("." . ,(expand-file-name
-                 (concat user-emacs-directory "backups")))))
-(setq auto-save-file-name-transforms
-      `((".*" ,temporary-file-directory t)))
-
-;; Save point position between sessions
-(require 'saveplace)
-(setq-default save-place t)
-(setq save-place-file (expand-file-name ".places" user-emacs-directory))
 
 ;; Emacs on OS X?
 (setq *is-mac* (equal system-type 'darwin))
 
 ;; Init packages
 (require 'init-package)
+(require 'init-backups)
+(require 'init-theme)
 
-;; Theme first
-(c42:require-packages 'color-theme-solarized)
-(c42:after-initializing
- (color-theme-solarized-dark)
- (set-default-font "Monaco 12"))
 (require 'init-base)
 
 ;; Sync packages
