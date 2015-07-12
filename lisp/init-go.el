59811bf374c42e4e04dcf55d087ed50c14707da1
Niranjan Paranjape <niranjangp@gmail.com>
2015-07-12 21:16:45 +0530

    Changing go mode hook

--- lisp/init-go.el
+++ lisp/init-go.el
@@ -1,16 +1,20 @@
-(c42:require-packages 'go-mode 'go-def 'go-company 'go-imports 'go-test)
+(c42:require-packages 'go-mode 'go-def 'go-autocomplete 'go-imports 'go-oracle
+		      'go-test 'go-rename)
 
 (defun c42:go-mode-hook ()
+
   (add-hook 'before-save-hook 'gofmt-before-save)
+
+  ;; Customize compile command to run go build
+  (if (not (string-match "go" compile-command))
+      (set (make-local-variable 'compile-command)
+           "go build -v"))
   (local-set-key (kbd "M-.") 'godef-jump)
   (local-set-key (kbd "M-,") 'pop-tag-mark)
-  (local-set-key (kbd "C-c ," 'go-test-current-file)))
+  (local-set-key (kbd "C-c ,") 'go-test-current-file))
 
 (c42:after-initializing
- (add-hook 'go-mode-hook 'c42:go-mode-hook)
- (add-hook 'go-mode-hook (lambda ()
-			   (set (make-local-variable 'company-backends) '(company-go))
-			   (company-mode))))
+ (add-hook 'go-mode-hook 'c42:go-mode-hook))
 
 (when *is-mac*
   (c42:after-initializing
